f7491e79bb1cea30400deafd8341b14842841b23
pshan0120 <pshan0120@naver.com>
2021-03-27 16:50:57 +0900

    Initial commit

---
+++ src/main/java/com/gaching/gachingwebservice/GachingWebserviceApplication.java
@@ -0,0 +1,13 @@
+package com.gaching.gachingwebservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GachingWebserviceApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(GachingWebserviceApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/gaching/gachingwebservice/GachingWebserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gaching.gachingwebservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GachingWebserviceApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
